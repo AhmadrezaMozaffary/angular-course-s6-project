b81119eb7d46a33187406636da7c201ee6b4330a
AhmadReza Mozaffary <AhmadRezaM@systemgroup.net>
2022-08-27 11:10:26 +0430

    Add some routing using custom events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-app';
+  visiblePage: string = 'recipes'
+
+  setActivePage(activePage: string){
+    this.visiblePage = activePage;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
+  @Output() page: EventEmitter<string> = new EventEmitter();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.page.emit('recipes');
   }
 
+  ngOnInit(): void {}
+
+  onSelect(page: string) {
+    this.page.emit(page);
+  }
 }
