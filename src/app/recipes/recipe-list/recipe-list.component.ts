9a302da7e46adf73c76ce36a4a175258f52064d9
AhmadReza Mozaffary <AhmadRezaM@systemgroup.net>
2022-08-27 12:24:20 +0430

    Seprate item's into their own component
    Get item's data by clicking on each item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -10,13 +10,25 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe(
-      'Test Recipe',
-      'This is a test',
+      'A delicious food',
+      'Iranian / Persian food ',
       'https://newsmedia.tasnimnews.com/Tasnim/Uploaded/Image/1400/09/07/1400090712354169924151294.jpg'
     ),
+    new Recipe(
+      'Steak beef',
+      'Medium rare steak beef',
+      'https://img.freepik.com/free-photo/juicy-steak-medium-rare-beef-with-spices-grilled-vegetables_2829-18672.jpg?w=1380&t=st=1661584012~exp=1661584612~hmac=08370b1836e327c86668631888b00d4341d025417907cea0f79cb207d9fedf7f'
+    ),
   ];
+
+  @Output() selecedRecipe: EventEmitter<Recipe> = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  getRecipeData(recipeData: Recipe) {
+    console.log(recipeData);
+    this.selecedRecipe.emit(recipeData)
+  }
 }
