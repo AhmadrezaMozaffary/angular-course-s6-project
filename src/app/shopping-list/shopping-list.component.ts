1e24480dcab9ffd675fb997c61776bd8d4fd0541
AhmadReza Mozaffary <AhmadRezaM@systemgroup.net>
2022-08-27 10:14:00 +0430

    Init |my-first-angular-project| repo

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.mode';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css'],
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatos', 10),
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
