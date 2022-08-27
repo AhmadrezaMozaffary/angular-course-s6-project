f3b8633c97f7e471969b0fef6bc02aa078aeeae1
AhmadReza Mozaffary <AhmadRezaM@systemgroup.net>
2022-08-27 12:56:54 +0430

    Update last commit

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -7,7 +7,7 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipes.component.css'],
 })
 export class RecipesComponent implements OnInit {
-  detailsData: Recipe = { name: '', description: '', imagePath: '' };
+  detailsData: Recipe | undefined;
 
   constructor() {}
 
